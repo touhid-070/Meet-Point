1de6b35da3648fecaa2d51e05f641b87b0a515c6
Touhid Ansari <touhidansari187@gmail.com>
2024-11-28 23:46:17 +0530

    Complete

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.png"
+          width={36}
+          height={36}
+          alt="meetpoint logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] pl-1 font-extrabold text-white max-sm:hidden">
+          Meet<span className='text-blue-500'>Point</span>
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
